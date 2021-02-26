faed619ace15ae9fcca8a8e5158cf01393e1d49d
Eric Stratton <strattonericj@gmail.com>
2021-02-26 11:41:42 -0800

    small changes, working to get a hold of Native components

--- find-wax/App.js
+++ find-wax/App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => console.log("Text Pressed");
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
