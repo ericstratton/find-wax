6557b3dbe82dd36e65f034fb89ae697c0cc70ad9
Eric Stratton <strattonericj@gmail.com>
2021-02-26 16:11:58 -0800

    add PaperProvider to render and move styles above render

--- find-wax/App.js
+++ find-wax/App.js
@@ -1,24 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper'; // UI library //
 
 export default function App() {
 
   const handlePress = () => console.log("Text Pressed");
   
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <PaperProvider>
+      <SafeAreaView style={styles.container}>
+        <Text numberOfLines={1} onPress={handlePress}>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
