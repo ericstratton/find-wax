10d625020d869ed29265f737dd233b0f2729abb6
Eric Stratton <strattonericj@gmail.com>
2021-03-05 09:30:18 -0800

    working on building screens

--- find-wax/src/screens/MainScreen.js
+++ find-wax/src/screens/MainScreen.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Map from './../components/Map';
 
 const Screen = () => {
   return (
-    <View>
-
+    <View style={style.body}>
+      <Text>Home Screen</Text>
+      <View>
+        <Map />
+      </View>
     </View>
   );
 };
+
+const style=StyleSheet.create({
+  body: {
+    backgroundColor: '#207178'
+  }
+})
 
 export default Screen;
--- find-wax/src/screens/WelcomeScreen.js
+++ find-wax/src/screens/WelcomeScreen.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 function WelcomeScreen() {
   return (
     <View>
-      <Text></Text>
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require('./../../assets/logo.png')} />
+        <Text>LOADING: WelcomeScreen</Text>
+      </View>
     </View>
-  )
+  );
 }
 
 export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 300,
+    height: 300
+  },
+  logoContainer: {
+    position: 'relative',
+    top: 70,
+    alignItems: 'center'
+  }
+})
 
