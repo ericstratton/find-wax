53e1625b6931a3f14a4426fdc31da904a75256b8
Eric Stratton <strattonericj@gmail.com>
2021-03-04 19:10:56 -0800

    add welcome and main screen files

---
+++ find-wax/src/screens/MainScreen.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Screen = () => {
+  return (
+    <View>
+
+    </View>
+  );
+};
+
+export default Screen;
---
+++ find-wax/src/screens/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function WelcomeScreen() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default WelcomeScreen;
+
