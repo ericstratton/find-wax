5893a3967dcc0c56b908efc0bc80281ac4dcbdd1
Eric Stratton <strattonericj@gmail.com>
2021-03-05 15:22:34 -0800

    comment out theme file for now

--- find-wax/src/config/theme.js
+++ find-wax/src/config/theme.js
@@ -1,10 +1,15 @@
-import { configureFonts, DefaultTheme } from 'react-native-paper';
-import customFonts from './fonts';
+// import { configureFonts, DefaultTheme } from 'react-native-paper';
+// // import colors from './colors';
+// import customFonts from './fonts';
 
-const theme = {
-  ...DefaultTheme,
-  fonts: configureFonts(customFonts),
-  roundness: 30,
-};
+// const theme = {
+//   ...DefaultTheme,
+//   fonts: configureFonts(customFonts),
+//   roundness: 30,
+//   colors: {
+//     primary: '#f5e9be',
+//     accent: '#207178',
+//   },
+// };
 
-export default theme;
+// export default theme;
