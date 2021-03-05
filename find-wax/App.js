8b2c1f42f192a94f6051889771074936d4d3469a
Eric Stratton <strattonericj@gmail.com>
2021-03-05 11:54:49 -0800

    remove all styles from App file

--- find-wax/App.js
+++ find-wax/App.js
@@ -1,32 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper'; // UI library //
-import theme from './src/custom-properties/Theme'; 
-import AppControl from './src/components/AppControl';
-import Map from './src/components/Map';
+import { Provider as PaperProvider } from 'react-native-paper';
+import theme from './src/custom-properties/Theme';
+import MainScreen from './src/screens/MainScreen';
+import WelcomeScreen from './src/screens/WelcomeScreen'; 
+
 
 export default function App() {
 
-  // const handlePress = () => console.log("Text Pressed");
 
   return (
     <PaperProvider theme={theme}>
-      <SafeAreaView style={styles.container}>
-        {/* <Text numberOfLines={1} onPress={handlePress}>Hello Everyone!</Text> */}
-        {/* <AppControl /> */}
-        <Map />
+      <SafeAreaView>
+        <WelcomeScreen />
+        {/* <MainScreen />  */}
         <StatusBar style="auto" />
       </SafeAreaView>
     </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
