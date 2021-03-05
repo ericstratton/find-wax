1b7ba799c272a9a53e6457a36150ec027d580c15
Eric Stratton <strattonericj@gmail.com>
2021-03-05 15:22:05 -0800

    remove react paper import and provider

--- find-wax/App.js
+++ find-wax/App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
-import theme from './src/custom-properties/Theme';
+// import { Provider as PaperProvider } from 'react-native-paper';
+import theme from './src/config/theme';
 import MainScreen from './src/screens/MainScreen';
 import WelcomeScreen from './src/screens/WelcomeScreen'; 
 
@@ -11,12 +11,10 @@ export default function App() {
 
 
   return (
-    <PaperProvider theme={theme}>
-      <SafeAreaView  >
-        {/* <WelcomeScreen /> */}
-        <MainScreen /> 
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    </PaperProvider>
+    <SafeAreaView  >
+      <WelcomeScreen />
+      {/* <MainScreen />  */}
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
