42d9860dd72e2b36ce60e96bc6bbc96a07f53b97
Eric Stratton <strattonericj@gmail.com>
2021-03-05 14:21:13 -0800

    remove unnecessary import statements

--- find-wax/src/components/Map.js
+++ find-wax/src/components/Map.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
 
 
 
 function Map() {
   return (
-    
     <MapView style={styles.map} />
-
   );
 }
 
 const styles = StyleSheet.create({
   map: {
-    width: 300,
-    height: 6000
+    width: 350,
+    height: 350
   }
 })
 
