e257041c3df7bff090fd9a4b25c055f0274d2915
Eric Stratton <strattonericj@gmail.com>
2021-03-05 09:53:23 -0800

    update button

--- find-wax/src/components/Button.js
+++ find-wax/src/components/Button.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-paper';
 
-export default function Button() {
+function AppButton() {
   return (
-    <Button
-      theme={{
-        roundness: 3,
-        fonts: { medium: 'Open Sans' }
-      }}
-      {...props}
-      >
-        Search
+    <Button style={styles.button}>
+      <Text style={styles.Text}>{buttonText}</Text>
     </Button>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '20%',
+    marginVertical: 10
+  },
+  text: {
+    fontSize: 20,
+    textTransform: 'uppercase',
+    fontWeight: 'bold'
+  }
+});
+
+export default AppButton;
