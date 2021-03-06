b7a2e0c8dca4f878eeec2efd2e783719f3f8ee50
Eric Stratton <strattonericj@gmail.com>
2021-03-06 11:14:48 -0800

    build reusable button component

---
+++ find-wax/src/components/AppButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+import colors from './../config/colors'
+
+function AppButton({ title, onPress }) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.secondary,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  text: {
+    color: colors.white,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  }});
+
+export default AppButton;
