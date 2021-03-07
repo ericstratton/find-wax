e328f4c4c91d58579ef09709b25ff0e6110fc8c1
Eric Stratton <strattonericj@gmail.com>
2021-03-06 17:41:20 -0800

    add reuable card component

---
+++ find-wax/src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import colors from '../config/colors'
+
+function Card({ title, subTitle }) {
+  return (
+    <View style={styles.card}>
+      <Text>{title}</Text>
+      <Text>{subTitle}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 10,
+    backgroundColor: colors.white,
+  }
+})
+export default Card;
