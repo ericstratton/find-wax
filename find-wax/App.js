17b12c7a64aab466551f48fd84889dccf4974e97
Eric Stratton <strattonericj@gmail.com>
2021-03-06 18:22:59 -0800

    use expo constants to move screen off status bar on android

--- find-wax/App.js
+++ find-wax/App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet, Platform } from 'react-native';
 // import { Provider as PaperProvider } from 'react-native-paper';
-import theme from './src/config/theme';
+// import theme from './src/config/theme';
 import MainScreen from './src/screens/MainScreen';
-import WelcomeScreen from './src/screens/WelcomeScreen'; 
+import WelcomeScreen from './src/screens/WelcomeScreen';
+import Constants from 'expo-constants';
+
 
 
 export default function App() {
-
-
   return (
-    <SafeAreaView  >
-      <WelcomeScreen />
-      {/* <MainScreen />  */}
+    <SafeAreaView style={styles.screen} >
+      {/* <WelcomeScreen /> */}
+      <MainScreen /> 
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: Constants.statusBarHeight,
+  }
+})
