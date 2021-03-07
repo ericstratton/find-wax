8fdd33cc777d481269f7e610342543e6caf93a75
Eric Stratton <strattonericj@gmail.com>
2021-03-07 15:28:01 -0800

    update client to use bearer auth - still being rejected

--- find-wax/src/api/client.js
+++ find-wax/src/api/client.js
@@ -1,7 +1,11 @@
 import { create } from 'apisauce';
 
 const apiClient = create({
-  baseURL: 'https://api.yelp.com/v3/businesses'
+  baseURL: 'https://api.yelp.com/v3/businesses',
+  headers: {
+    Authorization: 'bearer',
+    API_KEY: `${process.env.API_KEY}`
+  }
 });
 
 export default apiClient;
