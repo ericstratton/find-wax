75553cd7e69428585f2d63f7f96fc28c32f1c3b3
Eric Stratton <strattonericj@gmail.com>
2021-03-08 13:28:37 -0800

    add global styles to search navigator

--- find-wax/src/nav/SearchNavigator.js
+++ find-wax/src/nav/SearchNavigator.js
@@ -2,11 +2,16 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import WelcomeScreen from './../screens/WelcomeScreen';
 import MainScreen from '../screens/MainScreen';
+import colors from '../config/colors';
 
 const Stack = createStackNavigator();
 
 const SearchNavigator = () => (
-  <Stack.Navigator>
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: colors.primary },
+      headerTintColor: colors.white
+    }}>
     <Stack.Screen
       name="Welcome"
       component={WelcomeScreen}
