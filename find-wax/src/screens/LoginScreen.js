30940c5b3355df27d1846ef16d8ae8bade6e747c
Eric Stratton <strattonericj@gmail.com>
2021-03-08 14:58:14 -0800

    update to use formik to handle state

--- find-wax/src/screens/LoginScreen.js
+++ find-wax/src/screens/LoginScreen.js
@@ -1,30 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import { Formik } from 'formik';
 import AppButton from '../components/AppButton';
 import Screen from './../components/Screen';
 
 function LoginScreen(props) {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
 
   return (
     <Screen style={styles.container}>
-      <TextInput
-        autoCapitalize='none'
-        autoCorrect={false}
-        keyboardType='email-address'
-        onChangeText={text => setEmail(text)} 
-        placeholder='Email'
-      />
-      <TextInput 
-        autoCapitalize='none'
-        autoCorrect={false}
-        onChangeText={text => setPassword(text)}
-        placeholder='Password'
-        secureTextEntry={true}  
-      />
-      <AppButton title="Login" onPress={() => console.log(email, password)}/>
+
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        onSubmit={values => console.log(values)}
+      >
+        { ({ handleChange, handleSubmit }) => (
+          <>
+            <TextInput
+            autoCapitalize='none'
+            autoCorrect={false}
+            keyboardType='email-address'
+            onChangeText={handleChange('email')} 
+            placeholder='Email'
+            />
+            <TextInput 
+              autoCapitalize='none'
+              autoCorrect={false}
+              onChangeText={handleChange('password')}
+              placeholder='Password'
+              secureTextEntry={true}  
+            />
+            <AppButton title="Login" onPress={handleSubmit}/>
+          </>
+        )}
+      </Formik>
     </Screen>
   );
 }
