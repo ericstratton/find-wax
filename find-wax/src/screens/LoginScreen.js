f067e84724ecba8f811305aee9bbd36e855241da
Eric Stratton <strattonericj@gmail.com>
2021-03-08 14:37:38 -0800

    add static login screen

---
+++ find-wax/src/screens/LoginScreen.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import AppButton from '../components/AppButton';
+import Screen from './../components/Screen';
+
+function LoginScreen(props) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  return (
+    <Screen style={styles.container}>
+      <TextInput
+        autoCapitalize='none'
+        autoCorrect={false}
+        keyboardType='email-address'
+        onChangeText={text => setEmail(text)} 
+        placeholder='Email'
+      />
+      <TextInput 
+        autoCapitalize='none'
+        autoCorrect={false}
+        onChangeText={text => setPassword(text)}
+        placeholder='Password'
+        secureTextEntry={true}  
+      />
+      <AppButton title="Login" onPress={() => console.log(email, password)}/>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
+
+export default LoginScreen;
