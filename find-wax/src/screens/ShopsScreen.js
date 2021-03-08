ccf3d11592e62c38eda44dd921e0075e8e8d359e
Eric Stratton <strattonericj@gmail.com>
2021-03-08 10:58:56 -0800

    get flat list to render with shop data - WOOOHOOOgit add src/screens/git add src/screens/

--- find-wax/src/screens/ShopsScreen.js
+++ find-wax/src/screens/ShopsScreen.js
@@ -1,43 +1,46 @@
-import React, { useState, useEffect }from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 import { FlatList } from 'react-native';
 import Card from '../components/Card';
 import Button from './../components/AppButton';
 import shopsApi from './../api/shops';
+import useApi from './../hooks/useApi';
+import colors from '../config/colors';
+
 
 export default function ShopsScreen() {
-
-  const [shops, setShops] = useState([]);
-  const [error, setError] = useState(false);
+  const { data: shops, error, loading, request: loadShops } = useApi(shopsApi.getShops);
 
   useEffect(() => {
     loadShops();
   }, []);
 
-  const loadShops = async () => {
-    const response = await shopsApi.getShops();
-    if (!response.ok) return setError(true);
-    setError(false);
-    setShops(response.data);
-  };
-
   return (
-    <View>
+    <View style={styles.container}>
       {error && (<>
         <Text>Couldn't retrieve data.</Text>
         <Button title="Retry" onPress={loadShops}/>
       </>)}
+      <ActivityIndicator animating={loading} size='large' />
       <FlatList 
-        data={shops}
-        keyExtractor={listing => listing.id.toString()}
-        renderItem={({ item }) =>
+        data={shops.businesses}
+        keyExtractor={shop => shop.id.toString()}
+        renderItem={({ item }) => (
           <Card
-            title={item.title}
-            subTitle={item.subTitle} />
-          } />
-      <Text>{console.log(shops)}</Text>
+            title={item.name}
+            subTitle={item.phone} />
+        )} />
+      <Text></Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: colors.lightGrey
+  }
+})
+
+// object['businesses'][0][]
+// console.log(shops.businesses[0].name)
