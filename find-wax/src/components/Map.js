cf2f013c1aa87a0b66b4e621b8916585667d71de
Eric Stratton <strattonericj@gmail.com>
2021-03-09 09:09:15 -0800

    add test map marker

--- find-wax/src/components/Map.js
+++ find-wax/src/components/Map.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+
 
 
 
 function Map() {
   return (
-    <MapView style={styles.map} />
+    <>
+      <MapView style={styles.map}>
+        <Marker
+          style={styles.marker} 
+          coordinate={{
+            latitude: 45.513114,
+            longitude: -122.670843,
+          }}
+          image={require('./../../assets/custom-map-marker.png')}
+          title={'Test'} />
+      </MapView>
+    </>
   );
 }
 
@@ -14,6 +26,10 @@ const styles = StyleSheet.create({
   map: {
     width: 350,
     height: 350
+  },
+  marker: {
+    width: '100%',
+    height: 50,
   }
 })
 
