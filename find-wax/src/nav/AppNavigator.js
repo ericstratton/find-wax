73dbc92ee24fecff68855c6c167f8ba7e05f980a
Eric Stratton <strattonericj@gmail.com>
2021-03-09 08:17:20 -0800

    properly destructure props for material icons

--- find-wax/src/nav/AppNavigator.js
+++ find-wax/src/nav/AppNavigator.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
 import MainScreen from '../screens/MainScreen';
 import ShopsScreen from '../screens/ShopsScreen';
+import colors from '../config/colors';
+
 
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Shops" component={ShopsScreen} />
-    <Tab.Screen name="Home" component={MainScreen} />
+  <Tab.Navigator
+    tabBarOptions={{
+      activeBackgroundColor: colors.primary,
+      activeTintColor: colors.white,
+      inactiveBackgroundColor: colors.primary,
+      inactiveTintColor: colors.secondary
+    }} >
+    <Tab.Screen name="Shops" component={ShopsScreen} 
+      options={{
+        tabBarIcon: ({ color, size }) => 
+          <MaterialCommunityIcons name="shopping" color={color} size={size} />
+      }}/>
+    <Tab.Screen name="Home" component={MainScreen} 
+      options={{
+        tabBarIcon: ({ color, size }) => 
+          <MaterialCommunityIcons name="home" color={color} size={size} />
+      }}/>
   </Tab.Navigator>
 )
 
