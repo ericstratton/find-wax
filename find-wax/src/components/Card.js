774c50eea01393c1b660c080b244e831aa5b4815
Eric Stratton <strattonericj@gmail.com>
2021-03-08 16:41:43 -0800

    add imageUrl as a prop to card to display images from yelp

--- find-wax/src/components/Card.js
+++ find-wax/src/components/Card.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
 import colors from '../config/colors'
 
-function Card({ title, subTitle }) {
+function Card({ imageUrl, title, subTitle }) {
   return (
     <View style={styles.card}>
+      <Image style={styles.image} source={{ uri: imageUrl }} />
       <Text>{title}</Text>
       <Text>{subTitle}</Text>
     </View>
@@ -15,6 +16,11 @@ const styles = StyleSheet.create({
   card: {
     borderRadius: 10,
     backgroundColor: colors.white,
+    marginVertical: 10,
+  },
+  image: {
+    width: '100%',
+    height: 150,
   }
 })
 export default Card;
