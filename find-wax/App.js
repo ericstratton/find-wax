bb959a6d1cf1aa5f298afb7de10885aa0f5ceca9
Eric Stratton <strattonericj@gmail.com>
2021-03-09 16:56:35 -0800

    remove unused import statements

--- find-wax/App.js
+++ find-wax/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 // import { Provider as PaperProvider } from 'react-native-paper';
 // import theme from './src/config/theme';
-import MainScreen from './src/screens/MainScreen';
+import MapScreen from './src/screens/MapScreen';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import ShopsScreen from './src/screens/ShopsScreen';
 
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './src/nav/AuthNavigator';
 import AppNavigator from './src/nav/AppNavigator';
+import navTheme from './src/nav/navTheme';
+
 
 
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={navTheme}>
       {/* <AuthNavigator /> */}
       <AppNavigator />
     </NavigationContainer>
