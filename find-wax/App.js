eba5518a89b137bdccc6ec07a11d620da94b92d0
Eric Stratton <strattonericj@gmail.com>
2021-03-10 14:34:26 -0800

    add code to intialize firebase in app file

--- find-wax/App.js
+++ find-wax/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import firebaseConfig from './src/firebase';
+import * as firebase from 'firebase';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './src/nav/AuthNavigator';
 import AppNavigator from './src/nav/AppNavigator';
@@ -7,11 +8,17 @@ import navTheme from './src/nav/navTheme';
 
 
 
+
 export default function App() {
+  if(!firebase.apps.length) {
+    console.log('Connected to Firebase');
+    firebase.initializeApp(firebaseConfig);
+  }
+
   return (
     <NavigationContainer theme={navTheme}>
-      {/* <AuthNavigator /> */}
-      <AppNavigator />
+      <AuthNavigator />
+      {/* <AppNavigator /> */}
     </NavigationContainer>
   );
 }
