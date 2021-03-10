12a74c7da6bcad1c1e4ae7406f5b19ecffd65367
Eric Stratton <strattonericj@gmail.com>
2021-03-10 14:39:42 -0800

    send values from register form to firebase auth

--- find-wax/src/screens/RegisterScreen.js
+++ find-wax/src/screens/RegisterScreen.js
@@ -3,13 +3,16 @@ import { StyleSheet, Text } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
+
+import { registration } from './../api/auth';
 import AppButton from '../components/AppButton';
 import Screen from './../components/Screen';
 
 const validationSchema = Yup.object().shape({
-  name: Yup.string().required().label("Name"),
+  firstName: Yup.string().required().label("First Name"),
+  lastName: Yup.string().required().label("Last Name"),
   email: Yup.string().required().email().label("Email"),
-  password: Yup.string().required().min(5).label("Password")
+  password: Yup.string().required().min(6).label("Password")
 })
 
 function RegisterScreen() {
@@ -17,17 +20,23 @@ function RegisterScreen() {
   return (
     <Screen style={styles.container}>
       <Formik
-        initialValues={{ name: '', email: '', password: '' }}
-        onSubmit={values => console.log(values)} 
+        initialValues={{ firstName: '', lastName: '', email: '', password: '' }}
+        onSubmit={values => registration(values)} 
         validationSchema={validationSchema} >
         { ({ handleChange, handleSubmit, errors }) => (
           <>
             <TextInput
             autoCorrect={false}
-            onChangeText={handleChange('name')} 
-            placeholder='Name'
+            onChangeText={handleChange('firstName')} 
+            placeholder='First Name'
             />
-            <Text style={{ color: 'red' }}>{errors.name}</Text>
+            <Text style={{ color: 'red' }}>{errors.firstName}</Text>
+            <TextInput
+            autoCorrect={false}
+            onChangeText={handleChange('lastName')} 
+            placeholder='Last Name'
+            />
+            <Text style={{ color: 'red' }}>{errors.lastName}</Text>
             <TextInput
             autoCapitalize='none'
             autoCorrect={false}
