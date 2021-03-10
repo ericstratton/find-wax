7ecfd886f6bd307acda81beff4680ae7f99df377
Eric Stratton <strattonericj@gmail.com>
2021-03-09 16:59:32 -0800

    remove unused imports - didn't save all changes last commit

--- find-wax/App.js
+++ find-wax/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-// import { Provider as PaperProvider } from 'react-native-paper';
-// import theme from './src/config/theme';
-import MapScreen from './src/screens/MapScreen';
-import WelcomeScreen from './src/screens/WelcomeScreen';
-import ShopsScreen from './src/screens/ShopsScreen';
 
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './src/nav/AuthNavigator';
 import AppNavigator from './src/nav/AppNavigator';
 import navTheme from './src/nav/navTheme';
-
 
 
 
