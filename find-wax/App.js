4e80a654a44b97b37cedfb5c893bff4e34a670d7
Eric Stratton <strattonericj@gmail.com>
2021-03-10 16:12:57 -0800

    add conditional render for diff nav schemes

--- find-wax/App.js
+++ find-wax/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import firebaseConfig from './src/firebase';
 import * as firebase from 'firebase';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './src/nav/AuthNavigator';
 import AppNavigator from './src/nav/AppNavigator';
 import navTheme from './src/nav/navTheme';
+import AuthContext from './src/auth/context';
 
 
 
 
 export default function App() {
+  const [user, setUser] = useState()
+
   if(!firebase.apps.length) {
     console.log('Connected to Firebase');
     firebase.initializeApp(firebaseConfig);
   }
 
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      setUser(user);
+    } else {
+      console.log("Not working!");
+    }
+  });
+
   return (
-    <NavigationContainer theme={navTheme}>
-      <AuthNavigator />
-      {/* <AppNavigator /> */}
-    </NavigationContainer>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <NavigationContainer theme={navTheme}>
+        {user ? <AppNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
 
