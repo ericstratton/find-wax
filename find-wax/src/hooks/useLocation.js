2d5921b27aea52329c954924f7b70e1aa8bf3a95
Eric Stratton <strattonericj@gmail.com>
2021-03-14 12:36:46 -0700

    work out body of function

--- find-wax/src/hooks/useLocation.js
+++ find-wax/src/hooks/useLocation.js
@@ -4,5 +4,14 @@ import * as Location from 'expo-location';
 export default useLocation = () => {
   const [location, setLocation] = useState();
 
-  const getLocation = async
+  const getLocation = async () => {
+    try {
+      const { granted } = await Location.requestPermissionsAsync();
+      if (!granted) return;
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+        setLocation({ latitude, longitude });
+    }
+  }
 };
